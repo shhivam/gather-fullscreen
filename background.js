93cd3e002a8599688ad92f689948e1e153943e4b
Shivam <shivamsinghal5432@gmail.com>
2022-01-23 01:29:50 +0530

    Maximize the canvas on extension-icon-click

--- background.js
+++ background.js
@@ -1,1 +1,23 @@
 console.log("extension loaded");
+
+function mainThread(tab) {
+  console.log("here is your tab id", tab.id);
+  
+  const GAME_CANVAS_SELECTOR = "#root > div > div > div.Layout > div > div";
+  function maximizeGameCanvas() {
+    console.log("starting maximizing");
+    const gameCanvas = document.querySelector(GAME_CANVAS_SELECTOR);
+    console.log("here is game canvas", gameCanvas);
+    gameCanvas.requestFullscreen();
+  }
+
+  maximizeGameCanvas();
+}
+
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: mainThread,
+    args: [tab],
+  });
+});
